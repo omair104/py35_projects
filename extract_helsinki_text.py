9b70b44d461a105070b5aa52357b5974ee7a1f33
BabriO <BabriO@FM-9310655.br-edv.brnet.int>
2017-08-31 16:15:24 +0200

    Helsinki final

--- extract_helsinki_text.py
+++ extract_helsinki_text.py
@@ -27,116 +27,131 @@ for x in range(0, len(content)):
 
 
 '''
-extracted_tei = r'H:\circle\text_extractor\XML Helsinki Corpus Browser\XML Helsinki Corpus Browser\hcbrow\corpus\extracted_tei\428.txt'
 
-with open(extracted_tei, encoding='utf-8') as f:
-    content = f.readlines()
-    
 path = r'H:\circle\text_extractor\XML Helsinki Corpus Browser\XML Helsinki Corpus Browser\hcbrow\corpus\extracted'
-
-    
-tei= content[0]
-n= re.findall('n=".*?"',tei)[0][3:-1]
-id1 = re.findall('id=".*?"',tei)[0][4:-1]
-corpus_number = n+'_'+id1
-
-for x in range(0, len(content)):
-    while ('<title key' not in content[x]): 
-        x=x+1
-
-    title= re.findall('n=".*?"',content[x])[0][3:-1]
-    break
-
-for x in range(0, len(content)):
-    while ('<author key' not in content[x]): 
-        x=x+1
-
-    author= re.findall('key=".*?"',content[x])[0][5:-1]
-    break
-
-for x in range(0, len(content)):
-    while ('author_age' not in content[x]): 
-        x=x+1
-
-    authorage= re.findall('target=".*?"',content[x])[0][13:-1]
-    break
-
-for x in range(0, len(content)):
-    while ('<date type="manuscript"' not in content[x]): 
-        x=x+1
-
-    pubdate= re.findall('>.*?<',content[x])[0][1:-1]
-    break
-
-title_date=pubdate
-for x in range(0, len(content)):
-    while ('<title level="m">' not in content[x]): 
-        x=x+1
-    m = re.search("1", content[x])
-    #print(m.start())
-    
-    if m:
-        start= m.start()
-        title_date= content[x][start:-9]
-    break
-print(title_date)
-    
-opener_exists= False
-for x in range(0, len(content)):
-    if ('<opener>' not in content[x]): 
-        pass
-    else:
-        opener_exists= True
-
-
-
 file_number=0
 
-x=0
-while ('<text>' not in content[x]): 
-        x=x+1        
+extracted_tei = r'H:\circle\text_extractor\XML Helsinki Corpus Browser\XML Helsinki Corpus Browser\hcbrow\corpus\extracted_tei\428.txt'
+extracted_tei_dir = r'H:\circle\text_extractor\XML Helsinki Corpus Browser\XML Helsinki Corpus Browser\hcbrow\corpus\extracted_tei'
 
-while(x<len(content)-2):
-    file= os.path.join(path, str(x)+'.txt')
-    f= open(file, 'w+', encoding='utf-8')
-    file_number = file_number+1
-
-    written_header = '<file> <no=%s> <corpusnumber=%s> <corpus=helsinki_corpus_xml_edition> <title=%s> <author=%s> \
-<dialect=Early Modern English> <authorage=%s> <pubdate=%s> <genre1=let.non-priv> <genre2=X>  <encoding=utf-8> <text> \n\n' %(file_number,corpus_number, title, author, authorage, title_date)
-
-    f.write(written_header)
+files = os.listdir(extracted_tei_dir)
+for file in files:
+    extracted_tei2= os.path.join(extracted_tei_dir, file)
+    print(extracted_tei2)
     
-    if opener_exists:
-        while ('<opener>' not in content[x]): 
+    with open(extracted_tei2, encoding='utf-8') as f:
+        content = f.readlines()
+        
+    
+    tei= content[0]
+    n= re.findall('n=".*?"',tei)[0][3:-1]
+    id1 = re.findall('id=".*?"',tei)[0][4:-1]
+    corpus_number = n+'_'+id1
+    
+    for x in range(0, len(content)):
+        while ('<title key' not in content[x]): 
+            x=x+1
+    
+        title= re.findall('n=".*?"',content[x])[0][3:-1]
+        break
+    
+    for x in range(0, len(content)):
+        while ('<author key' not in content[x]): 
+            x=x+1
+    
+        author= re.findall('key=".*?"',content[x])[0][5:-1]
+        break
+    
+    for x in range(0, len(content)):
+        while ('author_age' not in content[x]): 
+            x=x+1
+    
+        authorage= re.findall('target=".*?"',content[x])[0][13:-1]
+        break
+    
+    for x in range(0, len(content)):
+        while ('<date type="manuscript"' not in content[x]): 
+            x=x+1
+    
+        pubdate= re.findall('>.*?<',content[x])[0][1:-1]
+        break
+    
+    title_date=pubdate
+    print(len(content))
+    for x in range(0, len(content)):
+        print(x)
+        while ('<title level="m"' not in content[x]): 
+            x=x+1
+            if x==len(content):
+                x=x-1
+                break
+        m = re.search("1", content[x])
+        #print(m.start())
+        
+        if m:
+            start= m.start()
+            title_date= content[x][start:-9]
+        break
+    print(title_date)
+        
+    opener_exists= False
+    for x in range(0, len(content)):
+        if ('<opener>' not in content[x]): 
+            pass
+        else:
+            opener_exists= True
+    
+    
+    
+    x=0
+    while ('<text' not in content[x]): 
+            x=x+1        
+    
+    while(x<len(content)-2):
+        file_number = file_number+1
+        file= os.path.join(path, str(file_number)+'.txt')
+        f= open(file, 'w+', encoding='utf-8')
+        
+    
+        written_header = '<file> <no=%s> <corpusnumber=%s> <corpus=helsinki_corpus_xml_edition> <title=%s> <author=%s> \
+    <dialect=Early Modern English> <authorage=%s> <pubdate=%s> <genre1=let.non-priv> <genre2=X>  <encoding=utf-8> <text> \n\n' %(file_number,corpus_number, title, author, authorage, title_date)
+    
+        f.write(written_header)
+        
+        if opener_exists:
+            while ('<opener>' not in content[x]): 
+                x=x+1
+                if x== len(content)-1:
+                    break
+            if x== len(content)-1:
+                f.close()
+                os.remove(file)
+                file_number = file_number-1
+                break
+            while ('</opener>' not in content[x]): 
+                f.write(content[x][7:])
+                x=x+1
+            f.write(content[x][7:])
+            
+    
+    
+        while ('<p>' not in content[x]): 
             x=x+1
             if x== len(content)-1:
                 break
         if x== len(content)-1:
             f.close()
             os.remove(file)
+            file_number = file_number-1
             break
-        while ('</opener>' not in content[x]): 
+        
+        while ('</p>' not in content[x]): 
             f.write(content[x][7:])
             x=x+1
         f.write(content[x][7:])
-        
-
-
-    while ('<p>' not in content[x]): 
+        f.write('\n</text> </file>')
         x=x+1
-        if x== len(content)-1:
-            break
-    if x== len(content)-1:
-        f.close()
-        os.remove(file)
-        break
     
-    while ('</p>' not in content[x]): 
-        f.write(content[x][7:])
-        x=x+1
-    f.write(content[x][7:])
-    f.write('\n</text> </file>')
-
-#print(written_header)
-
-f.close()
+    #print(written_header)
+    
+    f.close()
